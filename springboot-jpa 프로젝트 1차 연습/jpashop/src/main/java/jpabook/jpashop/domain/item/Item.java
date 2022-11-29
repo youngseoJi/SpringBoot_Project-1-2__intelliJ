fedb701d066315551bf331626561463864ffc36e
youngseoji <estell.ys.j@gmail.com>
2022-11-29 14:54:48 +0900

    #6 : Add / 상풍 도메인 - 상품 엔티티 개발 (비지니스 로직 추가)

--- springboot-jpa 프로젝트 1차 연습/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ springboot-jpa 프로젝트 1차 연습/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -24,16 +25,30 @@ public abstract class Item {
     private Long id; // 식별자 pk, 칼럼명 order_item_id 설정
 
     private String name;
-
     private int price;
-
-    private int stock;
-
-    private int stockQuantity;
+    private int stockQuantity; // 재고 개수
 
     // N:N 관계
      /* readonly(거울) item 에서는 값변경 X ->  categories 카테고리에 FK 외래키가 있음
        mappedBy="items" -> Categorey 테이블에 있는 categories필드에 의해 맵핑된 것*/
     @ManyToMany(mappedBy = "items")
     public List<Category> categories = new ArrayList<>();
+
+
+    // 비즈니스 로직
+
+    // 재고 증가
+    public  void  addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    // 재고 감소
+     public void removeStock(int quantity) {
+          int restStock = this.stockQuantity - quantity;
+
+          if (restStock < 0) {
+              throw new NotEnoughStockException("재고가 0보다 적으면 안된다.");
+          }
+          this.stockQuantity = restStock;
+     }
 }
