3d66284497d8f5202ec6cb8bc5ad8a35e2d3610c
youngseoji <estell.ys.j@gmail.com>
2022-12-02 13:48:41 +0900

    #10 : Update/[웹 계층 개발] - 상품 기능 : 상품 목록 조회 - 컨트롤러 - view

--- springboot-jpa 프로젝트 1차 연습/jpashop/src/main/java/jpabook/jpashop/controller/ItemController.java
+++ springboot-jpa 프로젝트 1차 연습/jpashop/src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.controller;
 
 import jpabook.jpashop.domain.item.Book;
+import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.service.ItemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -9,17 +10,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 
 // 상품 컨트롤러
 @Controller
 @RequiredArgsConstructor // 생성자 자동 생성
-//@RequestMapping("/items")
+@RequestMapping("/items")
 public class ItemController {
 
     private final ItemService itemService;
 
     // 상품 등록폼
-    @GetMapping("/items/new")
+    @GetMapping("/new")
     public String createForm(Model model) {
 
         model.addAttribute("form", new BookForm()); // BookForm 등록폼, 빈 객체 담기
@@ -27,7 +30,7 @@ public class ItemController {
     }
 
     // 상품 등록
-    @PostMapping("/items/new")
+    @PostMapping("/new")
     public String create(BookForm form) {
 
         // 책 객체 생성
@@ -46,5 +49,14 @@ public class ItemController {
         // 저장 후 리다이렉트 -
         return "redirect:/";
 
+    }
+
+    // 상품목록 조회
+    @GetMapping()
+    public String list(Model model) {
+        // 상품들을 저장소에서 꺼내 list[]인 items에 담음 -> model 에 저장
+        List<Item> items = itemService.findItems();
+        model.addAttribute("items", items);
+        return "items/itemList";  // itemList 상품목록 뷰 렌더링
     }
 }
